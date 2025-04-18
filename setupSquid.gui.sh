ea0925d2e3fcaf54bd526f4521923381cc9947a7
sukritcharredsoul <sukritchoudhary16@gmail.com>
2025-04-18 11:25:01 +0530

    Hello

--- setupSquid.gui.sh
+++ setupSquid.gui.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 WHITELIST="/etc/squid/whitelist.txt"
+BLACKLIST="/etc/squid/blocked_sites.txt"
 
-# Ask for a domain to add
+
 domain=$(zenity --entry \
   --title="Add to Whitelist" \
   --text="Enter the domain you want to allow (e.g. example.com):")
 
+
+domainB=$(zenity --entry \
+  --title="Add to Blacklist" \
+  --text="Enter the domain you want to block from Access:")
+
+
+changed=false
+
 if [[ -n "$domain" ]]; then
     echo "$domain" | sudo tee -a "$WHITELIST" > /dev/null
+    changed=true
+fi
+
+if [[ -n "$domainB" ]]; then
+    echo "$domainB" | sudo tee -a "$BLACKLIST" > /dev/null
+    changed=true
+fi
+
+if [[ "$changed" = true ]]; then
     sudo systemctl restart squid
     zenity --info \
       --title="Success" \
-      --text="$domain has been added to whitelist and Squid was restarted."
+      --text="Updated Squid configuration: Whitelist: $domain Blacklist: $domainB"
 else
     zenity --warning --text="No domain entered!"
 fi
--- setupSquid.sh
+++ setupSquid.sh
@@ -8,16 +8,32 @@ sudo apt install -y squid
 
 
 echo "Storing the original Squid.conf File for backups."
+
 sudo cp /etc/squid/squid.conf /etc/squid/squid.conf.bak
 
 
-echo "example.com" | sudo tee /etc/squid/whitelist.txt 
+echo "examples.com" | sudo tee /etc/squid/whitelist.txt 
+echo "youtube.com" | sudo tee /etc/squid/blocked_sites.txt
+
+echo "example.com" | sudo tee /etc/squid/blocked_sites.txt
 
 sudo tee /etc/squid/squid.conf > /dev/null <<EOL
+
+# Define allowed domains (whitelist)
 acl whitelist dstdomain "/etc/squid/whitelist.txt"
 http_access allow whitelist
+
+# Deny access to all other domains by default
 http_access deny all
+
+# Define the proxy port
 http_port 3128
+
+# Block specific domains (blocked sites)
+acl blocked dstdomain "/etc/squid/blocked_sites.txt"
+http_access deny blocked
+
+
 EOL
 
 sudo systemctl restart squid
